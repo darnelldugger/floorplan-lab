0e22716e6412c5bc8301ac1971c4ce930b444ca5
Darnell Dugger <darnelldugger@gmail.com>
2022-03-14 19:12:53 -0400

    initial commit

---
+++ src/FloorPlan.jsx
@@ -0,0 +1,20 @@
+import Kitchen from "./Kitchen";
+import LivingRoom from "./LivingRoom";
+import Bedroom from "./Bedroom";
+import Bath from "./Bath";
+
+const FloorPlan = (props) => {
+  return ( 
+    <>
+    <Kitchen />
+    <LivingRoom />
+    <Bath size = {'Half'} />
+    <Bedroom bedNum = {1} />
+    <Bedroom bedNum = {2} />
+    <Bedroom bedNum = {3} />
+    <Bath size = {'Full'} />
+    </>
+   )
+}
+ 
+export default FloorPlan;
