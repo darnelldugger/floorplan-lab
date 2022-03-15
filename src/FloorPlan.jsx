71e53fa6ed91c2a814620f1eb8abca0a9773ad8c
Darnell Dugger <darnelldugger@gmail.com>
2022-03-15 08:55:52 -0400

    add styling to floorplan

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -6,22 +6,22 @@ import Bath from "./Bath";
 const FloorPlan = (props) => {
   return ( 
     <>
-    <Kitchen />
     <LivingRoom />
-    <div className="half-bath">
-    <Bath size = {'Half'} />
-    </div>
     <div className="bedroom-1">
     <Bedroom bedNum = {1} />
+    </div>
+    <Kitchen />
+    <div className="half-bath">
+    <Bath size = {'Half'} />
     </div>
     <div className="bedroom-2">
     <Bedroom bedNum = {2} />
     </div>
-    <div className="bedroom-3">
-    <Bedroom bedNum = {3} />
-    </div>
     <div className="full-bath">
     <Bath size = {'Full'} />
+    </div>
+    <div className="bedroom-3">
+    <Bedroom bedNum = {3} />
     </div>
     </>
    )
